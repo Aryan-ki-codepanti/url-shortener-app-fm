3f0433d94adfcd1699fc82bb57aee635042e80fc
Aryan Sethi <aryan.rdps@gmail.com>
2024-08-05 16:37:51 +0530

    Added Navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Navbar.css";
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="container">
+            <div className="Navbar">
+                <div className="logo">
+                    <span>Shortly</span>
+                </div>
+
+                <div className="nav-links">
+                    <a href="#">Features</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Resources</a>
+                </div>
+
+                <div className="login-sign">
+                    <a href="#">Login</a>
+                    <button className="green-btn">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
