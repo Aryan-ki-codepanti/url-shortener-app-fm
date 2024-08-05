96b7c61b54013e4b9c9d92e40c09422bed719e17
Aryan Sethi <aryan.rdps@gmail.com>
2024-08-05 15:53:57 +0530

    Added font awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import FA from "react-fontawesome";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <FA className="super-crazy-colors" name="rocket" size="4x" />
         </div>
     );
 }
