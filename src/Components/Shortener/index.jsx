c469605e6995b3d1f1bed5470426c67005cb4e6b
Aryan Sethi <aryan.rdps@gmail.com>
2024-08-07 16:19:01 +0530

    Added shortener copy functionality and listing

--- src/Components/Shortener/index.jsx
+++ src/Components/Shortener/index.jsx
@@ -5,14 +5,35 @@ const Shortener = () => {
     const [error, setError] = useState(false);
     const [inputLink, setInputLink] = useState("");
 
-    const handleShorten = e => {
+    // [{ url: str , shortened: str, copied: bool }..]
+    const [links, setLinks] = useState([
+        {
+            url: "https://www.frontendmentor.io/",
+            shortened: "https://rel.ink/k4LKyk",
+            copied: false
+        },
+        {
+            url: "https://www.frontendmentor.io/",
+            shortened: "https://rel.ink/k4LKyk",
+            copied: false
+        }
+    ]);
+
+    const handleShorten = async e => {
         if (!inputLink) {
             setError(prev => true);
             return;
         }
+
         setError(prev => false);
     };
 
+    const handleOnCopy = (e, i) => {
+        const linksCopy = structuredClone(links);
+        linksCopy[i] = { ...linksCopy[i], copied: true };
+        setLinks(prev => linksCopy);
+        navigator.clipboard.writeText(linksCopy[i].shortened);
+    };
     return (
         <div className="Shortener">
             <div className="container">
@@ -30,7 +51,27 @@ const Shortener = () => {
                     {error && <span>Please add a link</span>}
                 </div>
 
-                <div className="shortened-links"></div>
+                <div className="shortened-links">
+                    {links.map((link, i) => (
+                        <div key={i} className="link">
+                            <span>{link.url}</span>
+                            <span>{link.shortened}</span>
+
+                            {link.copied ? (
+                                <button className="green-btn copied">
+                                    Copied!
+                                </button>
+                            ) : (
+                                <button
+                                    className="green-btn"
+                                    onClick={e => handleOnCopy(e, i)}
+                                >
+                                    Copy
+                                </button>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
